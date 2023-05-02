2269714f30dfc3c8d9eff702cdb0e73925387027
Subhomoy Pal <subhomoy.pal@webskitters.com>
2023-05-02 19:55:52 +0530

    add config file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-latest'`, () => {
+  it(`should have as title 'jira'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-latest');
+    expect(app.title).toEqual('jira');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-latest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'jira app is running!'
+    );
   });
 });
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  exports: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, MenuCardComponent],
+  exports: [HeaderComponent, MenuCardComponent],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
