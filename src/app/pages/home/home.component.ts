c895b1700d6ecd30134dc7fb82ad54246b35b7fe
webskitters <webskitters@webskitterss-iMac-kaushik.local>
2023-05-04 20:34:35 +0530

    design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { DefaultComponent } from '@project/modal';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
-
+  addprojectModal() {
+    this.dialog
+      .open(DefaultComponent, {
+        data: {
+          type: 'addproject',
+          data: '',
+          filterData: '',
+        },
+      })
+      .afterClosed()
+      .subscribe((result) => {});
+  }
 }
