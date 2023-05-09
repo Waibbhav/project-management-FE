58c360baeb9513a631ac85db7446e625f109cd84
webdevatn <webdevwdc@gmail.com>
2023-05-09 13:21:22 +0530

    design

--- src/app/modal/modal.module.ts
+++ src/app/modal/modal.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default/default.component';
 import { MaterialModule } from '@project/material.module';
-
+import { NgScrollbarModule } from 'ngx-scrollbar';
 @NgModule({
   declarations: [DefaultComponent],
   entryComponents: [DefaultComponent],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    NgScrollbarModule,
+
   ]
 })
 export class ModalModule { }
