23ab4b7a502a566440637bcbc3929a7ede8b7fc2
Rounak Barman <rounak.barman@webskitters.com>
2023-05-09 13:30:43 +0530

    html

--- src/app/pages/backlog/backlog.component.ts
+++ src/app/pages/backlog/backlog.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backlog.component.scss']
 })
 export class BacklogComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
