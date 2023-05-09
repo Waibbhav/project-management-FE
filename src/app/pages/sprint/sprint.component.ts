eae222adc1678050150b7d3b6f6a9a18cd0391c7
waibbhavanand.singh <waibbhavanand.singh@webskitters.in>
2023-05-09 14:36:32 +0530

    modal creation

--- src/app/pages/sprint/sprint.component.ts
+++ src/app/pages/sprint/sprint.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { DefaultComponent } from '@project/modal';
 @Component({
   selector: 'app-sprint',
   templateUrl: './sprint.component.html',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SprintComponent implements OnInit {
   panelOpenState = false;
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  assignTask(){
+    this.dialog
+    .open(DefaultComponent, {
+      data: {
+        type: 'assigntask',
+        data: '',
+        filterData: '',
+      },
+    })
+    .afterClosed()
+    .subscribe((result) => {});
+  }
+  sprintdetails(){
+    this.dialog
+    .open(DefaultComponent, {
+      data: {
+        type: 'sprint-details',
+        data: '',
+        filterData: '',
+      },
+    })
+    .afterClosed()
+    .subscribe((result) => {});
   }
  
 }
