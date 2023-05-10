308c8b70f5ec24d3285be4ce392156266d3ede99
webdevatn <webdevwdc@gmail.com>
2023-05-10 11:50:41 +0530

    design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jira'`, () => {
+  it(`should have as title 'WTS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jira');
+    expect(app.title).toEqual('WTS');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'jira app is running!'
+      'WTS app is running!'
     );
   });
 });
