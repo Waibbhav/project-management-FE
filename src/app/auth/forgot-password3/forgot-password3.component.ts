69fce62136fd77be2227789668b251ea0f61acc0
waibbhavanand.singh <waibbhavanand.singh@webskitters.in>
2023-05-15 16:43:12 +0530

    dev

--- src/app/auth/forgot-password3/forgot-password3.component.ts
+++ src/app/auth/forgot-password3/forgot-password3.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password3.component.scss']
 })
 export class ForgotPassword3Component implements OnInit {
-
+hidepass:boolean= false
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  hidePassword(){
+this.hidepass = !this.hidepass
+  }
 }
