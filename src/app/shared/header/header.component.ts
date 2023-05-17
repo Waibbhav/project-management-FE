46fe793afc998df79e6d6f144cc11a5b2c81b57c
sreya <sreya.malakar@webskitters.in>
2023-05-17 12:56:36 +0530

    dev

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DefaultComponent } from '@project/modal';
+import {FormGroup,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,9 +10,17 @@ import { DefaultComponent } from '@project/modal';
 export class HeaderComponent implements OnInit {
   searchText:boolean=false
   drop:boolean=false
+  removeX1:boolean=false
+  text:string=''
+  addForm!:FormGroup
+
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.addForm=new FormGroup({
+     search:new FormControl('') 
+    })
+
   }
 
   addprojectModal(){
@@ -26,5 +35,22 @@ export class HeaderComponent implements OnInit {
       .afterClosed()
       .subscribe((result) => {});
   }
+  removeText(){
+    this.removeX1=false
+    this.addForm.reset()
+  
+  }
+  removeX(e:any){
+    console.log(e.target.value);
+    
+    if(e.target.value!='')
+    {
+      this.removeX1=true
+    }
+    else{
+      this.removeX1=false
+    }
+  }
+  
   
 }
