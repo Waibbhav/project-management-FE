c42f2150559876ef42892c3a092394091071c66c
Waibbhav <Singhwaibbhav@gmail.com>
2025-01-05 15:03:28 +0530

    dev

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -8,28 +8,27 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { EventService } from './event.service';
+import { map } from 'rxjs/operators'; // Import map operator
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   constructor(private event: EventService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    this.event.isLogedin.subscribe((res: boolean): any => {
-      if (!res) {
-        //return false;
-        this.router.navigate(['/login']);
-      } else {
+  ): Observable<boolean | UrlTree> | boolean | UrlTree {
+    return this.event.isLogedin.pipe(
+      map((isLoggedIn: boolean) => {
+        if (!isLoggedIn) {
+          // Navigate to login if not logged in
+          return this.router.createUrlTree(['/login']);
+        }
+        // Allow navigation if logged in
         return true;
-      }
-    });
-    return true;
+      })
+    );
   }
 }
