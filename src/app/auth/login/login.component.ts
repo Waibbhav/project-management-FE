14c44c0a3cfcbfb537d026e0f3990784fef1917e
Waibbhav <Singhwaibbhav@gmail.com>
2025-01-05 14:06:56 +0530

    dev

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '@project/services/api.service'; // Adjust path to your ApiService
 import { StorageService } from '@project/services/storage.service';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
