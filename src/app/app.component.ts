7f5409f70e69421c7474d683a33c7cbcafc01b75
Waibbhav <Singhwaibbhav@gmail.com>
2025-01-05 14:17:43 +0530

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'WTS';
+export class AppComponent implements OnInit {
+
+  title="WTS"
+  constructor(private router: Router, private storageService: StorageService) {}
+
+  ngOnInit() {
+    if (this.storageService.isAuthenticate()) {
+      this.router.navigate(['/home']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
